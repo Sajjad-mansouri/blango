822b7c5914a578dc7bccb5892384b3f44f41e647
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-12 12:05:31 +0000

    Finish django configurations

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericRelation
+
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
