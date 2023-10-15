92adbe88e73b97c36c9a7dc1b71d57c0ea7d0039
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-15 16:55:26 +0000

    Finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
-from blog.models import Post,Tag
+from blog.models import Post,Tag,Comment
 from blango_auth.models import User
 
+class TagField(serializers.SlugRelatedField):
+  def to_internal_value(self,data):
+    try:
+      return self.get_queryset().get_or_create(value=data.lower())[0]
+    except (TypeError,ValueError):
+      self.fail(f'Tag value {data} is invalid')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+  id=serializers.IntegerField(required=False)
+  creator=UserSerializer(read_only=True)
+
+  class Meta:
+    model=Comment
+    field=["id", "creator", "content", "modified_at", "created_at"]
+    readonly = ["modified_at", "created_at"]
+
+
 class PostSerializer(serializers.ModelSerializer):
-  tags = serializers.SlugRelatedField(
+  tags = TagField(
         slug_field="value", many=True, queryset=Tag.objects.all()
     )
 
@@ -16,7 +39,23 @@ class PostSerializer(serializers.ModelSerializer):
     readonly=['modified_at','created_at']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["first_name", "last_name", "email"]
+
+class PostDetailSerializer(PostSerializer):
+  comments=CommentSerializer(many=True)
+
+  def update(self,instance,validate_data):
+    comments=validate_data.pop('comments')
+
+    instance=super().update(instance,validate_data)
+
+    for comment_data in comments:
+      if comment_data.get('id'):
+        #comment has an id was pre-existing
+        continue
+      
+      comment=Comment(**comment)
+      comment.creator=self.context['request'].user
+      content.content_object=instance
+      comment.save()
+    
+    return instance
