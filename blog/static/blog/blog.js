e6867b9796f8ac3ae9ba5224939eec8d5a0d3dbf
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-18 10:58:26 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,8 +1,24 @@
-const theNumber = 1
-
-if (theNumber === 1) {
-  let name = 'Leo'
-  alert(name)
+for (let i=0;i<10;i+=1){
+  console.log('for loop i: '+i)
 }
 
-alert(name)
+let j=0
+while(j<10){
+  console.log('while loop j: '+j)
+  j+=1
+}
+
+let k=0
+do{
+  console.log('do while k: '+k)
+  k+=1
+}while(k<10)
+
+const numbers=[0,1,2,3,4,5,6,7,8,9]
+numbers.forEach(value=>{
+  console.log('for each value '+value)
+})
+
+const doubled=numbers.map(value=>value*2)
+console.log('here are the doubled numbers')
+console.log(doubled)
